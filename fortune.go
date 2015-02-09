1677c86e028f319062fe2aba73efaa2abf427f79
chee <chee@snaek.org>
2015-02-09 12:16:29 +0000

    seed the golang rand

--- fortune.go
+++ fortune.go
@@ -5,9 +5,11 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"strings"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano() + 100);
 	text, _ := ioutil.ReadFile("fortunes")
 	fortunes := strings.Split(string(text), "\n%\n")
 	fmt.Println(fortunes[rand.Intn(len(fortunes))])
